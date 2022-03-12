8137b5f1b7ad55be0508f6411c4bf09c14e66f50
Matheus Alves <matheusalves.bmd@gmail.com>
2022-03-11 23:52:46 -0300

    Provide animations

---
+++ src/lotties/index.js
@@ -0,0 +1,26 @@
+import amazingAnimationData from "./amazing.json";
+import awefulAnimationData from "./aweful.json";
+import badAnimationData from "./bad.json";
+import goodAnimationData from "./good.json";
+import okOkAnimationData from "./ok-ok.json";
+
+import { reactions } from "../components/EmojiFeedback/data";
+
+const selectAnimationData = reaction => {
+  switch (reactions) {
+    case reactions[0]:
+      return awefulAnimationData;
+    case reactions[1]:
+      return badAnimationData;
+    case reactions[2]:
+      return okOkAnimationData;
+    case reactions[3]:
+      return goodAnimationData;
+    case reactions[4]:
+      return amazingAnimationData;
+    default:
+      return null;
+  }
+};
+
+export default selectAnimationData;
